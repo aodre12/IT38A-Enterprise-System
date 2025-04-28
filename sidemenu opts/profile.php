c514a70d8dcf49c96249dfcf07b43e6aa53aece2
Danmark12 <audreyabigailhisanza.9@gmail.com>
2025-04-28 21:19:16 +0800

    sdmn opts profile added

---
+++ sidemenu opts/profile.php
@@ -0,0 +1,14 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Profile</title>
+</head>
+<body>
+    <h1>My Profile</h1>
+    <p>View and edit your profile information here.</p>
+    <a href="user_sidemenu.php">← Back to Menu</a>
+</body>
+</html>
