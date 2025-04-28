95a396e8a489485d3ded51db6dda9c1280e15745
Clintharolcodes <sumampongclintharold@gmail.com>
2025-04-28 15:09:43 +0800

    added signup

---
+++ signup.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="tile">
+        <h2>Sign Up</h2>
+        <form method="POST" action="signup_process.php">
+            <input type="text" name="username" placeholder="Username" class="input-field" required><br>
+            <input type="email" name="email" placeholder="Email" class="input-field" required><br>
+            <input type="password" name="password" placeholder="Password" class="input-field" required><br>
+            <button type="submit" class="button">Sign Up</button>
+        </form>
+    </div>
+</body>
+</html>
