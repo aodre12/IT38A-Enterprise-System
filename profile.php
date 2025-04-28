d67cee101fdb1ca0bbb67f6b8d9caa95f702a8c5
Clintharolcodes <sumampongclintharold@gmail.com>
2025-04-28 15:11:41 +0800

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="tile">
+        <h2>Profile</h2>
+        <form method="POST" action="profile_update.php">
+            <input type="text" name="name" placeholder="Full Name" class="input-field" required><br>
+            <input type="email" name="email" placeholder="Email Address" class="input-field" required><br>
+            <input type="text" name="phone" placeholder="Phone Number" class="input-field" required><br>
+            <button type="submit" class="button">Update Profile</button>
+        </form>
+    </div>
+</body>
+</html>
