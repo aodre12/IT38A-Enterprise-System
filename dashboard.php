d8d474b80db6d158549afd414766bb55380cf025
Clintharolcodes <sumampongclintharold@gmail.com>
2025-04-28 15:10:17 +0800

    dashboard added

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="tile">
+        <h2>Dashboard</h2>
+        <p>Welcome to your dashboard!</p>
+        <a href="profile.php" class="button">Go to Profile</a><br><br>
+        <a href="sidebar.php" class="button">Open Sidebar</a><br><br>
+        <a href="login.php" class="button">Logout</a>
+    </div>
+</body>
+</html>
