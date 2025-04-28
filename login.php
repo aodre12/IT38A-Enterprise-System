78f623ee264216ea09c2dda1fe6381faec150779
Clintharolcodes <sumampongclintharold@gmail.com>
2025-04-28 15:33:31 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +6,11 @@
     <title>Login</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body class="background">
+<body>
+    <div class="header"></div>
     <div class="tile">
         <h2>Login</h2>
-        <form method="POST" action="login_process.php">
+        <form method="POST" action="dashboard.php">
             <input type="text" name="username" placeholder="Username" class="input-field" required><br>
             <input type="password" name="password" placeholder="Password" class="input-field" required><br>
             <button type="submit" class="button">Login</button>
