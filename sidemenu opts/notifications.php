4be04398fb9e7b129fee795ff880232751405916
Danmark12 <audreyabigailhisanza.9@gmail.com>
2025-04-28 21:20:06 +0800

    Create notifications.php

---
+++ sidemenu opts/notifications.php
@@ -0,0 +1,14 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Notifications</title>
+</head>
+<body>
+    <h1>Notifications</h1>
+    <p>View your latest notifications here.</p>
+    <a href="user_sidemenu.php">← Back to Menu</a>
+</body>
+</html>
