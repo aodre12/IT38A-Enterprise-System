a89b78deab72b31e9280860b7395089808de6256
Danmark12 <audreyabigailhisanza.9@gmail.com>
2025-04-28 21:17:36 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,14 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Home Page</h1>
+    <p>Welcome to the Home page!</p>
+    <a href="user_sidemenu.php">← Back to Menu</a>
+</body>
+</html>
