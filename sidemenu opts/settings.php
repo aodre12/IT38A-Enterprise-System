d20402a64bba5139019b03e87c3590b400caf310
Danmark12 <audreyabigailhisanza.9@gmail.com>
2025-04-28 21:20:54 +0800

    Create settings.php

---
+++ sidemenu opts/settings.php
@@ -0,0 +1,14 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Settings</title>
+</head>
+<body>
+    <h1>Settings</h1>
+    <p>Manage your account settings here.</p>
+    <a href="user_sidemenu.php">← Back to Menu</a>
+</body>
+</html>
