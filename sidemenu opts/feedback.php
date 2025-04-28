fad78af3e067a56747a099a3bbb687c8c9675535
Danmark12 <audreyabigailhisanza.9@gmail.com>
2025-04-28 21:23:19 +0800

    Create feedback.php

---
+++ sidemenu opts/feedback.php
@@ -0,0 +1,14 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Feedback</title>
+</head>
+<body>
+    <h1>Feedback</h1>
+    <p>We value your feedback! Send us your thoughts.</p>
+    <a href="user_sidemenu.php">← Back to Menu</a>
+</body>
+</html>
